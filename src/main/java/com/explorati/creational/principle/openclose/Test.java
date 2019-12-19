eb3f597ba075d0c5f94934956dac1301e546a657
explorati <w1748419447@163.com>
2019-12-19 15:57:37 +0800

    开闭原则测试 原理

--- src/main/java/com/explorati/creational/principle/openclose/Test.java
+++ src/main/java/com/explorati/creational/principle/openclose/Test.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 /**
  * @ Author : Weijian_Wang
  * @ Date : Created in 15:17 2019/12/19 0019
- * @ Description ：
+ * @ Description ：对扩展(子类扩展父类方法)开放，对修改(修改顶级接口)关闭
  */
 public class Test {
     public static void main(String[] args) {
