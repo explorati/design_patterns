b9e81ca22c6d374c4088cc56799928562831b9f8
explorati <w1748419447@163.com>
2019-12-19 15:54:32 +0800

    顶级接口类

---
+++ src/main/java/com/explorati/creational/principle/openclose/ICourse.java
@@ -0,0 +1,14 @@
+package com.explorati.creational.principle.openclose;
+
+import java.math.BigDecimal;
+
+/**
+ * @ Author : Weijian_Wang
+ * @ Date : Created in 15:14 2019/12/19 0019
+ * @ Description ：
+ */
+public interface ICourse {
+    Integer getId();
+    String getName();
+    BigDecimal getPrice();
+}
