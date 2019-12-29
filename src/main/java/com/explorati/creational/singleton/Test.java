4f8accaa68dd5690cdd9621fc8e72eff44bc3993
explorati <w1748419447@163.com>
2019-12-29 12:59:43 +0800

    反射攻击测试

--- src/main/java/com/explorati/creational/singleton/Test.java
+++ src/main/java/com/explorati/creational/singleton/Test.java
@@ -1,5 +1,8 @@
 package com.explorati.creational.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * @ Author : Weijian_Wang
  * @ Date : Created in 22:20 2019/12/20 0020
@@ -7,14 +10,23 @@ package com.explorati.creational.singleton;
  */
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 //        DoubleCheckSingleton doubleCheckSingleton = DoubleCheckSingleton.getInstance();
 //        System.out.println(doubleCheckSingleton.getClass());
 //        DoubleCheckSingleton doubleCheckSingleton1 = DoubleCheckSingleton.getInstance();
 //        System.out.println(doubleCheckSingleton1.getClass());
 
-        EnumSingleton singleton = EnumSingleton.getInstance();
-        singleton.test();
+//        EnumSingleton singleton = EnumSingleton.getInstance();
+//        singleton.test();
+
+        Singleton singleton = Singleton.getInstance();
+        System.out.println(singleton);
+
+        Class objectClass = Singleton.class;
+//        Class.forName(Singleton.class.getName());
+        Constructor constructor = objectClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        System.out.println(constructor.newInstance());
 
     }
 }
