69cef694c8ccfc924edf3b9c0a89eecc00a94a21
explorati <w1748419447@163.com>
2019-12-29 12:58:31 +0800

    懒汉式防止反射调用 及 特殊情况

--- src/main/java/com/explorati/creational/singleton/DoubleCheckSingleton.java
+++ src/main/java/com/explorati/creational/singleton/DoubleCheckSingleton.java
@@ -10,6 +10,9 @@ public class DoubleCheckSingleton {
     private volatile static DoubleCheckSingleton instance;
 
     private DoubleCheckSingleton() {
+        /**
+         * 防止反射调用：但是懒汉式加载创建实例时看时机；假如反射先调用，则创建两个不同对象，无法避免
+         */
         if(instance != null) {
             throw new IllegalArgumentException("单例模式禁止反射调用");
         }
